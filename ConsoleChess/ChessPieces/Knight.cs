03745450f5afb2adeddb006f71d4a4bb99fb57c4
Jonas <heschl@protonmail.com>
2020-10-04 23:22:23 +0200

    Implement CanMove for Knight

--- ConsoleChess/ChessPieces/Knight.cs
+++ ConsoleChess/ChessPieces/Knight.cs
@@ -6,7 +6,35 @@ namespace ConsoleChess.ChessPieces
     {
         public override (bool canMove, string errorMessage) CanMove(int xTo, int yTo, List<ChessPiece> pieces)
         {
-            // TODO implement this method
+            // Check whether the given coordinates are different from the current coordinates
+            if (!IsMoving(xTo, yTo))
+            {
+                return (false, "This is the initial position of the chosen chess piece");
+            }
+
+            // Check whether the given coordinates are inside the chess playing field
+            if (MovesBeyondBoard(xTo, yTo))
+            {
+                return (false, "This position is beyond the chess playing field");
+            }
+
+            var xOk = xTo - 2 == X || xTo - 1 == X || xTo + 1 == X || xTo + 2 == X;
+            var yOk = yTo - 2 == Y || yTo - 1 == Y || yTo + 1 == Y || yTo + 2 == Y;
+            if (!(xOk && yOk))
+            {
+                return (
+                    false,
+                    "The can only move to the any of the closest squares that are not on the same rank, file or diagonal"
+                );
+            }
+            
+            // Checks whether the given coordinates are blocked by a friendly piece
+            if (MovesOntoFriendly(xTo, yTo, pieces))
+            {
+                return (false, "A friendly piece is at that position");
+            }
+            
+            // All checks successful, can move is true
             return (true, null);
         }
 
