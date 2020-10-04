1fe4398db9ea7259a755ae4552ef024d658c8755
Jonas <heschl@protonmail.com>
2020-10-04 15:54:46 +0200

    Initialize chess board (ChessBoard::_chessPieces)

--- ConsoleChess/ChessBoard.cs
+++ ConsoleChess/ChessBoard.cs
@@ -11,16 +11,44 @@ namespace ConsoleChess
 
         public ChessBoard()
         {
-            _chessPieces.Add(new Rook(){Colour = ChessColour.Black, X = 0, Y = 0});
-            // TODO add all chess pieces
+            // -=-=-=- Initialize Chess Board -=-=-=-
+            // White pieces
+            _chessPieces.Add(new Rook {Colour = ChessColour.White, X = 0, Y = 0});
+            _chessPieces.Add(new Knight {Colour = ChessColour.White, X = 1, Y = 0});
+            _chessPieces.Add(new Bishop {Colour = ChessColour.White, X = 2, Y = 0});
+            _chessPieces.Add(new Queen {Colour = ChessColour.White, X = 3, Y = 0});
+            _chessPieces.Add(new King {Colour = ChessColour.White, X = 4, Y = 0});
+            _chessPieces.Add(new Bishop {Colour = ChessColour.White, X = 5, Y = 0});
+            _chessPieces.Add(new Knight {Colour = ChessColour.White, X = 6, Y = 0});
+            _chessPieces.Add(new Rook {Colour = ChessColour.White, X = 7, Y = 0});
+            // White pawns
+            foreach (var i in Enumerable.Range(0, 7))
+            {
+                _chessPieces.Add(new Pawn {Colour = ChessColour.White, X = i, Y = 1});
+            }
+            
+            // Black pieces
+            _chessPieces.Add(new Rook {Colour = ChessColour.Black, X = 0, Y = 7});
+            _chessPieces.Add(new Knight {Colour = ChessColour.Black, X = 1, Y = 7});
+            _chessPieces.Add(new Bishop {Colour = ChessColour.Black, X = 2, Y = 7});
+            _chessPieces.Add(new Queen {Colour = ChessColour.Black, X = 3, Y = 7});
+            _chessPieces.Add(new King {Colour = ChessColour.Black, X = 4, Y = 7});
+            _chessPieces.Add(new Bishop {Colour = ChessColour.Black, X = 5, Y = 7});
+            _chessPieces.Add(new Knight {Colour = ChessColour.Black, X = 6, Y = 7});
+            _chessPieces.Add(new Rook {Colour = ChessColour.Black, X = 7, Y = 7});
+            // Black pawns
+            foreach (var i in Enumerable.Range(0, 7))
+            {
+                _chessPieces.Add(new Pawn {Colour = ChessColour.Black, X = i, Y = 6});
+            }
         }
 
         public (bool success, string errorMessage) Move(int xFrom, int yFrom, int xTo, int yTo)
         {
             var piece = (
-             from pcs in _chessPieces
-             where pcs.X == xFrom && pcs.Y == yFrom
-             select pcs
+                from pcs in _chessPieces
+                where pcs.X == xFrom && pcs.Y == yFrom
+                select pcs
             ).GetEnumerator().Current;
 
             // If no piece could be found on the given position (xFrom, yFrom), return false.
@@ -29,14 +57,14 @@ namespace ConsoleChess
                 return (false, "The given position is empty");
             }
 
-            var canMove = piece.CanMove(xTo, yTo, _chessPieces);
+            var (canMove, errorMessage) = piece.CanMove(xTo, yTo, _chessPieces);
             // If moving the chosen piece to the chosen field is not allowed according to the CanMove method implemented
             // in each ChessPiece subclass, return false.
-            if (!canMove.canMove)
+            if (!canMove)
             {
-                return (false, canMove.errorMessage);
+                return (false, errorMessage);
             }
-            
+
             // All checks succeeded, return true.
             return (true, null);
         }
@@ -60,20 +88,20 @@ namespace ConsoleChess
         {
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
-            
+
             PrintTerminalLine();
-            
+
             foreach (var i in Enumerable.Range(2, 7).Reverse())
             {
                 Console.Write(i.ToString());
                 PrintPieceLine();
-                
+
                 PrintPieceSeparatorLine();
             }
-            
+
             Console.Write("1");
             PrintPieceLine();
-            
+
             PrintTerminalLine();
             PrintAbcLine();
         }
@@ -86,13 +114,13 @@ namespace ConsoleChess
         private static void PrintTerminalLine()
         {
             Console.Write(" ");
-            
+
             var consoleBackground = Console.BackgroundColor;
             var consoleForeground = Console.ForegroundColor;
 
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            
+
             Console.WriteLine("-----------------------------------------");
 
             Console.BackgroundColor = consoleBackground;
@@ -111,7 +139,7 @@ namespace ConsoleChess
 
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            
+
             Console.WriteLine("|    |    |    |    |    |    |    |    |");
 
             Console.BackgroundColor = consoleBackground;
@@ -126,13 +154,13 @@ namespace ConsoleChess
         private static void PrintPieceSeparatorLine()
         {
             Console.Write(" ");
-            
+
             var consoleBackground = Console.BackgroundColor;
             var consoleForeground = Console.ForegroundColor;
 
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.DarkGray;
-            
+
             Console.WriteLine("|----+----+----+----+----+----+----+----|");
 
             Console.BackgroundColor = consoleBackground;
@@ -147,13 +175,13 @@ namespace ConsoleChess
         private static void PrintAbcLine()
         {
             Console.Write(" ");
-            
+
             var consoleBackground = Console.BackgroundColor;
             var consoleForeground = Console.ForegroundColor;
 
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
-            
+
             Console.WriteLine("  a    b    c    d    e    f    g    h   ");
 
             Console.BackgroundColor = consoleBackground;
